a397162644045f7379e5e41225cbd49a8e8aca03
Mridang Agarwalla <mridang@nosto.com>
2018-12-21 02:18:04 +0530

    Added prettier support

--- handler.js
+++ handler.js
@@ -1,11 +1,10 @@
-const charter = require('./lib/charts')
-const exporter = require('highcharts-export-server')
+const charter = require("./lib/charts");
+const exporter = require("highcharts-export-server");
 
 module.exports.graph = async (event, context) => {
-
   try {
     var data;
-    if (event.httpMethod === 'GET') {
+    if (event.httpMethod === "GET") {
       data = JSON.parse(event.queryStringParameters.data);
     } else {
       data = JSON.parse(event.body);
@@ -14,36 +13,36 @@ module.exports.graph = async (event, context) => {
     console.error(err);
     return {
       headers: {
-        "Content-Type": 'text/plain'
+        "Content-Type": "text/plain"
       },
       statusCode: 400,
       isBase64Encoded: false,
       body: "Bad Request"
-    }
+    };
   }
 
   try {
-    exporter.initPool({ maxWorkers: 1 })
+    exporter.initPool({ maxWorkers: 1 });
     const chart = await charter.createChart(exporter, data);
     exporter.killPool();
 
     return {
       headers: {
-        "Content-Type": 'image/png'
+        "Content-Type": "image/png"
       },
       statusCode: 200,
       isBase64Encoded: true,
       body: chart
-    }
+    };
   } catch (err) {
     console.error(err);
     return {
       headers: {
-        "Content-Type": 'text/plain'
+        "Content-Type": "text/plain"
       },
       statusCode: 500,
       isBase64Encoded: false,
       body: "An error occurred"
-    }
+    };
   }
-}
+};
